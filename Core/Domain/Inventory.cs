45157caff28854632ab94dda89f1eb28e032f940
am205379@gmail.com <am205379@gmail.com>
2024-12-16 09:01:54 +0330

    Remove warehouse with dependencies

--- Core/Domain/Inventory.cs
+++ Core/Domain/Inventory.cs
@@ -2,7 +2,7 @@ namespace WarehouseManagement.Models
 {
     public class Inventory
     {
-       public int Id { get; set; }
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int WarehouseId { get; set; }
--- Core/Domain/Warehouse.cs
+++
@@ -1,9 +0,0 @@
-namespace WarehouseManagement.Models
-{
-    public class WarehouseI
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
-    }
-}
