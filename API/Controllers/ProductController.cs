f147fe19cc7fca7fb32df1983b343020f628f01f
am205379@gmail.com <am205379@gmail.com>
2024-12-16 10:38:13 +0330

    Append CRUD Product to ProductController

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -23,12 +23,36 @@ namespace Warehouse.Application.API.Controllers
             return Ok(products);
         }
 
+        // POST: api/v1/<ProductController>/{productId}
+        [HttpPost("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int productId)
+        {
+            var product = await _productRepository.Get(productId);
+            return Ok(product);
+        }
+
         // POST: api/v1/<ProductController>
         [HttpPost]
         public async Task<ActionResult> AddProduct(Product product)
         {
             await _productRepository.Add(product);
             return CreatedAtAction(nameof(GetAllProducts), new { id = product.Id }, product);
+        }
+
+        // DELETE: api/v1/<ProductController>/{ProductId}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(Product product)
+        {
+            await _productRepository.Delete(product);
+            return Ok();
+        }
+
+        // PUT: api/v1/<ProductController>/{ProductId}
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(Product product)
+        {
+            await _productRepository.Update(product);
+            return Ok();
         }
     }
 }
