b3cfb4ef1eae72b5de142b531fb842517494944b
am205379@gmail.com <am205379@gmail.com>
2024-12-16 09:25:49 +0330

    Remove unused methods in Product Repository & Interface

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using Warehouse.Infarstructure.Interfaces;
+using WarehouseManagement.Models;
 
 namespace Warehouse.Application.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
-    class ProductController : ControllerBase
+    public class ProductController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
 
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            var products = await _productRepository.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct(Product product)
+        {
+            await _productRepository.AddProductAsync(product);
+            return CreatedAtAction(nameof(GetAllProducts), new { id = product.Id }, product);
+        }
     }
 }
--- Infarstructure/Interfaces/IProductInterface.cs
+++ Infarstructure/Interfaces/IProductInterface.cs
@@ -4,10 +4,6 @@ namespace Warehouse.Infarstructure.Interfaces
 {
     public interface IProductRepository : IGenericeInterface<Product>
     {
-        Task<IEnumerable<Product>> GetAllProductsAsync();
-        Task<Product> GetProductByIdAsync(int id);
-        Task AddProductAsync(Product product);
-        Task UpdateProductAsync(Product product);
-        Task DeleteProductAsync(int id);
+        //
     }   
 }
