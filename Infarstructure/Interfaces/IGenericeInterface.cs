4b2e3726bc8a8eb5edcd33cba4e626747018cf0d
am205379@gmail.com <am205379@gmail.com>
2024-12-16 09:21:33 +0330

    Modify Generice Repository & Interface

--- Infarstructure/Interfaces/IGenericeInterface.cs
+++ Infarstructure/Interfaces/IGenericeInterface.cs
@@ -3,6 +3,7 @@ namespace Warehouse.Infarstructure.Interfaces
     public interface IGenericeInterface<T> where T : class
     {
         Task<List<T>> GetAll();
+        Task<T> Get(int id);
         Task Add(T entity);
         Task Update(T entity);
         Task Delete(T entity);
--- Infarstructure/Repositories/GenericeRepository.cs
+++ Infarstructure/Repositories/GenericeRepository.cs
@@ -1,31 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 using Warehouse.Infarstructure.Interfaces;
+using WarehouseManagement.Data;
 
 namespace Warehouse.Infarstructure.Repository
 {
     public class GenericRepository<T> : IGenericeInterface<T> where T : class
     {
-        private readonly DbContext _context;
+        private readonly WarehouseDbContext _context;
 
-        public GenericRepository(DbContext context)
+        public GenericRepository(WarehouseDbContext context)
         {
             _context = context;
         }
 
         public async Task<List<T>> GetAll()
         {
-            return await _context.Set<T>().ToListAsync();
+           return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> Get(int id)
+        {
+           return await _context.Set<T>().FindAsync(id);
         }
 
         public async Task Add(T entity)
         {
-            await _context.Set<T>().AddAsync(entity);
+            await _context.AddAsync(entity);
             await _context.SaveChangesAsync();
         }
 
         public async Task Update(T entity)
         {
-            _context.Set<T>().Update(entity);
+            _context.Update(entity);
             await _context.SaveChangesAsync();
         }
 
