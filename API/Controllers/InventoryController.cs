c9aa883167826c4523155efb290ca3b46a7b0186
am205379@gmail.com <am205379@gmail.com>
2024-12-16 08:52:11 +0330

    Implement Inventory Controller

---
+++ API/Controllers/InventoryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Infarstructure.Interfaces;
+using WarehouseManagement.Models;
+
+namespace Warehouse.Application.API.Controllers
+{
+    public class InventoryController : ControllerBase
+    {
+        
+    }
+}
