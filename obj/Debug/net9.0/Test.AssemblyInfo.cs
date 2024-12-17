2b526b091310f295bfc9ab2b85445eb7f98c75b5
am205379@gmail.com <am205379@gmail.com>
2024-12-17 08:30:57 +0330

    Implement Inventory DTOs

--- obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb3c6a30be6ca068655a8f2c4a99d7843ad2b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6743c2c8a5d600aa80f6395727ef748c120f494f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
